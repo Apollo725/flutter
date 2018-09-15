34c84a3988f28f048cb970754fd9016513aaf7f3
Apollo725 <natashazaharova725@outlook.com>
2018-09-15 11:46:50 +1000

    added username column, user profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'home' => 'pages#home'
-  get 'profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
   get 'explore' => 'pages#explore'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
